c18cc9b3a4f18336657ae71670815175aae98488
Rachel Sharp <rachel@hashicorp.com>
2021-04-02 17:41:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,3 +71,7 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP of the web server"
 }
+
+output "security_groups" {
+  value = [aws_instance.example.vpc_security_group_ids]
+}
