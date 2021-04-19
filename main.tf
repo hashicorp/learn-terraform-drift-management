90a521999e4c3e4a432b6ffcc4eb0dfb754ffc22
Rachel Sharp <rachel@hashicorp.com>
2021-04-19 15:27:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,16 +62,3 @@ resource "aws_security_group" "sg_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-output "instance_id" {
-  value = aws_instance.example.id
-}
-
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the web server"
-}
-
-output "security_groups" {
-  value = [aws_instance.example.vpc_security_group_ids]
-}
